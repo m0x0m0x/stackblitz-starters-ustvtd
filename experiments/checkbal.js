1a856cfc51d3859afbfe3b22d1a81f4146f9c03f
m0x0m0x <mx0mx@tuta.io>
2024-02-21 09:04:48 +0530

    add web3

---
+++ experiments/checkbal.js
@@ -0,0 +1,14 @@
+/*
+This if for checking the balance
+Referring to this documentation 
+*/
+const { Web3 } = require("web3");
+
+//private RPC endpoint
+const web3 = new Web3("https://rpc.ankr.com/polygon");
+
+//or public RPC endpoint
+//const web3 = new Web3('https://eth.llamarpc.com');
+
+web3.eth.getBlockNumber().then(console.log);
+// ↳ 18849658n
